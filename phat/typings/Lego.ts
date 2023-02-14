6ce3a8678f969a0dee3cfe91e063623f42ef3aaa
Shelven Zhou <favoring.zhou49@gmail.com>
2023-02-14 05:44:15 +0000

    project init

---
+++ phat/typings/Lego.ts
@@ -0,0 +1,50 @@
+import type * as PhalaSdk from "@phala/sdk";
+import type * as DevPhase from "devphase";
+import type * as DPT from "devphase/etc/typings";
+import type { ContractCallResult, ContractQuery } from "@polkadot/api-contract/base/types";
+import type { ContractCallOutcome, ContractOptions } from "@polkadot/api-contract/types";
+import type { Codec } from "@polkadot/types/types";
+
+export namespace Lego {
+    /** */
+    /** Queries */
+    /** */
+    namespace ContractQuery {
+        export interface Get extends DPT.ContractQuery {
+            (certificateData: PhalaSdk.CertificateData, options: ContractOptions): DPT.CallResult<DPT.CallOutcome<DPT.IJson<boolean>>>;
+        }
+    }
+
+    export interface MapMessageQuery extends DPT.MapMessageQuery {
+        get: ContractQuery.Get;
+    }
+
+    /** */
+    /** Transactions */
+    /** */
+    namespace ContractTx {
+        export interface Flip extends DPT.ContractTx {
+            (options: ContractOptions): DPT.SubmittableExtrinsic;
+        }
+    }
+
+    export interface MapMessageTx extends DPT.MapMessageTx {
+        flip: ContractTx.Flip;
+    }
+
+    /** */
+    /** Contract */
+    /** */
+    export declare class Contract extends DPT.Contract {
+        get query(): MapMessageQuery;
+        get tx(): MapMessageTx;
+    }
+
+    /** */
+    /** Contract factory */
+    /** */
+    export declare class Factory extends DevPhase.ContractFactory {
+        instantiate<T = Contract>(constructor: "new", params: [boolean], options?: DevPhase.InstantiateOptions): Promise<T>;
+        instantiate<T = Contract>(constructor: "default", params: never[], options?: DevPhase.InstantiateOptions): Promise<T>;
+    }
+}
