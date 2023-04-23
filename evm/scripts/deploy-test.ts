948e02c41398049c1a08a6bc3ff611872abe0952
Shelven Zhou <favoring.zhou49@gmail.com>
2023-04-23 17:14:56 +0000

    evm: update TestLensOracle

--- evm/scripts/deploy-test.ts
+++ evm/scripts/deploy-test.ts
@@ -1,21 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const TestOracle = await ethers.getContractFactory("TestOracle");
+  const TestLensOracle = await ethers.getContractFactory("TestLensOracle");
 
   const [deployer] = await ethers.getSigners();
 
   console.log('Deploying...');
   const attestor = deployer.address;  // When deploy for real e2e test, change it to the real attestor wallet.
-  const oracle = await TestOracle.deploy(attestor);
+  const oracle = await TestLensOracle.deploy(attestor);
   await oracle.deployed();
   console.log('Deployed', {
     oracle: oracle.address,
   });
 
   console.log('Configuring...');
-  await oracle.connect(deployer).request("bitcoin/usd");
-  await oracle.connect(deployer).registerFeed(0, "polkadot/usd");
+  await oracle.connect(deployer).request("0x01");
   console.log('Done');
 }
 
