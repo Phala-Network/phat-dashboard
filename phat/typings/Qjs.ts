5d6ff11778aa2d848dd0211d176bc9ac4b8b5944
Shelven Zhou <favoring.zhou49@gmail.com>
2023-06-04 12:00:17 +0000

    phat: update to qjs 1.0.0

--- phat/typings/Qjs.ts
+++ phat/typings/Qjs.ts
@@ -1,24 +1,47 @@
 import type * as PhalaSdk from "@phala/sdk";
-import type * as DevPhase from "devphase";
-import type * as DPT from "devphase/etc/typings";
+import type * as DevPhase from "@devphase/service";
+import type * as DPT from "@devphase/service/etc/typings";
 import type { ContractCallResult, ContractQuery } from "@polkadot/api-contract/base/types";
 import type { ContractCallOutcome, ContractOptions } from "@polkadot/api-contract/types";
 import type { Codec } from "@polkadot/types/types";
 
 export namespace Qjs {
-    type Qjs_Qjs_Output = { String: string } | { Bytes: number[] } | { Undefined: null };
-    type Result = { Ok: Qjs_Qjs_Output } | { Err: string };
+    type InkPrimitives_LangError$3 = {
+        CouldNotReadInput? : null
+        };
+    type Result$1 = {
+        Ok? : never[],
+        Err? : InkPrimitives_LangError$3
+        };
+    type Qjs_Qjs_Output$6 = {
+        String? : string,
+        Bytes? : number[] | string,
+        Undefined? : null
+        };
+    type Result$5 = {
+        Ok? : Qjs_Qjs_Output$6,
+        Err? : string
+        };
+    type Result$4 = {
+        Ok? : Result$5,
+        Err? : InkPrimitives_LangError$3
+        };
+    type InkPrimitives_Types_AccountId$7 = any;
+    type InkPrimitives_Types_Hash$8 = any;
+    type InkEnv_Types_NoChainExtension$9 = {
+
+        };
 
     /** */
     /** Queries */
     /** */
     namespace ContractQuery {
         export interface Eval extends DPT.ContractQuery {
-            (certificateData: PhalaSdk.CertificateData, options: ContractOptions, js: string, args: string[]): DPT.CallResult<DPT.CallOutcome<DPT.IJson<Result>>>;
+            (certificateData: PhalaSdk.CertificateData, options: ContractOptions, js: string, args: string[]): DPT.CallResult<DPT.CallOutcome<DPT.IJson<Result$4>>>;
         }
 
         export interface EvalBytecode extends DPT.ContractQuery {
-            (certificateData: PhalaSdk.CertificateData, options: ContractOptions, bytecode: number[], args: string[]): DPT.CallResult<DPT.CallOutcome<DPT.IJson<Result>>>;
+            (certificateData: PhalaSdk.CertificateData, options: ContractOptions, bytecode: number[] | string, args: string[]): DPT.CallResult<DPT.CallOutcome<DPT.IJson<Result$4>>>;
         }
     }
 
