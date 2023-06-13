0c8e67b7f8ffc8283289668ffda4dfafa2211f67
Kevin Wang <wy721@qq.com>
2023-06-13 01:10:53 +0000

    Workaround for cargo-contract

--- phat/contracts/lego/build.rs
+++ phat/contracts/lego/build.rs
@@ -2,9 +2,23 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR env var is not set");
-
-    let command_string = format!("cd {}/js && yarn && yarn build", manifest_dir);
+    // The following commands works with cargo check but does not work with cargo contract build.
+    //
+    // let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR env var is not set");
+    // let command_string = format!("cd {}/js && yarn && yarn build", manifest_dir);
+    //
+    // Workaround:
+    let manifest_dir =
+        env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR env var is not set");
+    let command_string = if std::path::Path::new(&manifest_dir).join("js").exists() {
+        format!("cd {}/js && yarn && yarn build", manifest_dir)
+    } else {
+        let root_dir = env::var("OLDPWD").expect("OLDPWD env var is not set");
+        format!(
+            "cd {}/phat/contracts/lego/js && yarn && yarn build",
+            root_dir
+        )
+    };
 
     let status = Command::new("sh")
         .arg("-c")
